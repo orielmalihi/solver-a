2eb9144172c0a4b081cda1dbe16ec41df3ebefde
oriel <orielmalihi1@gmail.com>
2020-04-28 23:30:24 +0300

    demo is working completly

--- solver.cpp
+++ solver.cpp
@@ -79,9 +79,10 @@ RealVariable &operator+(RealVariable &x, double num)
         return x;
     }
 }
-    RealVariable &operator+(double num, RealVariable &x){
-        return x + num;
-    }
+RealVariable &operator+(double num, RealVariable &x)
+{
+    return x + num;
+}
 
 RealVariable &operator-(RealVariable &x, double num)
 {
@@ -107,24 +108,26 @@ RealVariable &operator+(RealVariable &x, RealVariable &y)
 }
 RealVariable &operator-(RealVariable &x, RealVariable &y)
 {
-    return x + ((-1)*y);
+    return x + ((-1) * y);
 }
 
 RealVariable &operator==(RealVariable &x, RealVariable &y)
 {
     return x - y;
 }
-RealVariable &operator==(RealVariable& x, double num){
+RealVariable &operator==(RealVariable &x, double num)
+{
     return x - num;
 }
 
-double solve(RealVariable &x){
+double solve(RealVariable &x)
+{
     double ans = -1;
     if (x.coef_2 != 0)
     {
-        int a = x.coef_2;
-        int b = x.coef_1;
-        int c = x.coef_0;
+        double a = x.coef_2;
+        double b = x.coef_1;
+        double c = x.coef_0;
         // cout << "a = " << coef_2 << ", b = " << coef_1 << ", c = " << coef_0 << endl;
         int t = b * b - 4 * a * c;
         if (t < 0)
@@ -144,43 +147,180 @@ double solve(RealVariable &x){
     }
     else
     {
+        delete x.add;
         throw runtime_error("ERR: cant do this math. wrong input");
     }
     return ans;
 }
 
-// to do!!!!!
+//************** friend comp **********************************************
 
-// ************** friend comp **********************************************
+ComplexVariable &operator*(double num, ComplexVariable &x)
+{
+    if (x.add == NULL)
+    {
+        ComplexVariable *temp = new ComplexVariable(0, num);
+        temp->add = temp;
+        return *temp;
+    }
+    else
+    {
+        x.coef_2 *= num;
+        x.coef_1 *= num;
+        x.comp *= num;
+        return x;
+    }
+}
 
-// ComplexVariable &operator*(double num, ComplexVariable& x)
-// {
-//     if(x.ind == 0){
-//        ComplexVariable *temp = new ComplexVariable(0, num, 'c');
-//     temp->adr[temp->ind++] = temp;
-//     return *temp;
-//     }
-//     else{
-//         x.coef_2 *= num;
-//         x.coef_1 *= num;
-//         x.comp *= num;
-//         return x;
-//     }
+ComplexVariable &operator^(ComplexVariable &x, double num)
+{
+    if (x.add == NULL)
+    {
+        ComplexVariable *temp = new ComplexVariable(1, 0);
+        temp->add = temp;
+        return *temp;
+    }
+    else
+    {
+        x.coef_2 = x.coef_1 * x.coef_1;
+        x.coef_1 = 0;
+        x.comp *= x.comp;
+        return x;
+    }
+}
+ComplexVariable &operator/(ComplexVariable &x, double num)
+{
+    if (x.add == NULL)
+    {
+        double t = 1 / num;
+        ComplexVariable *temp = new ComplexVariable(0, t);
+        temp->add = temp;
+        return *temp;
+    }
+    else
+    {
+        x.coef_2 /= num;
+        x.coef_1 /= num;
+        x.comp /= num;
+        return x;
+    }
+}
+ComplexVariable &operator+(ComplexVariable &x, double num)
+{
+    if (x.add == NULL)
+    {
+        ComplexVariable *temp = new ComplexVariable(0, 1);
+        temp->comp = num + 0i;
+        temp->add = temp;
+        return *temp;
+    }
+    else
+    {
+        x.comp += num;
+        return x;
+    }
+}
+ComplexVariable &operator+(ComplexVariable &x, complex<double> y)
+{
+    if (x.add == NULL)
+    {
+        ComplexVariable *temp = new ComplexVariable(0, 1);
+        temp->comp = y;
+        temp->add = temp;
+        return *temp;
+    }
+    else
+    {
+        x.comp += y;
+        return x;
+    }
+}
+ComplexVariable &operator+(double num, ComplexVariable &x)
+{
+    return x + num;
+}
 
-// }
+ComplexVariable &operator-(ComplexVariable &x, double num)
+{
+    return x + (-1) * num;
+}
+ComplexVariable &operator+(ComplexVariable &x, ComplexVariable &y)
+{
+    if (x.add == NULL)
+    {
+        ComplexVariable *temp = new ComplexVariable(y.coef_2, y.coef_1 + 1);
+        temp->comp = y.comp;
+        temp->add = temp;
+        delete y.add;
+        return *temp;
+    }
+    else
+    {
+        x.coef_2 += y.coef_2;
+        x.coef_1 += y.coef_1;
+        x.comp += y.comp;
+        delete y.add;
+        return x;
+    }
+}
+ComplexVariable &operator-(ComplexVariable &x, ComplexVariable &y)
+{
+    return x + ((-1) * y);
+}
 
-// ComplexVariable &operator^(ComplexVariable& x, double num)
-// {
-//     ComplexVariable *temp = new ComplexVariable(1, 0, 'c');
-//     temp->adr[temp->ind++] = temp;
-//     return *temp;
-// }
-// ComplexVariable &operator/(ComplexVariable& x, double num)
-// {
-//     double t = 1 / num;
-//     ComplexVariable *temp = new ComplexVariable(0, t, 'c');
-//     temp->adr[temp->ind++] = temp;
-//     return *temp;
-// }
+ComplexVariable &operator==(ComplexVariable &x, ComplexVariable &y)
+{
+    return x - y;
+}
+ComplexVariable &operator==(ComplexVariable &x, double num)
+{
+    return x - num;
+}
+
+complex<double> solve(ComplexVariable &x)
+{
+    complex<double> ans = 0. + 0i;
+    if (x.coef_2 != 0)
+    {
+        double a = x.coef_2;
+        double b = x.coef_1;
+        complex<double> c = x.comp;
+        ans = (b * b - 4.0 * a * c);
+        ans = sqrt(ans) - b;
+        ans /= a * 2;
+        delete x.add;
+    }
+    else if (x.coef_1 != 0)
+    {
+        ans = (x.comp / x.coef_1) * (-1.0);
+        delete x.add;
+    }
+    else
+    {
+        delete x.add;
+        throw runtime_error("ERR: cant do this math. wrong input");
+    }
+    if (ans.imag() == 0)
+    {
+        ans.imag(0);
+    }
+    if (ans.real() == 0)
+    {
+        ans.real(0);
+    }
+    return ans;
+}
 
 } // namespace solver
+
+ostream &operator<<(ostream& os, complex<double> comp)
+{
+    if (comp.imag() >= 0)
+    {
+        return (os << comp.real() << "+" << comp.imag() << "i");
+    }
+    else
+    {
+        return (os << comp.real() << comp.imag() << "i");
+    }
+}
--- solver.hpp
+++ solver.hpp
@@ -33,7 +33,7 @@ public:
                                                     coef_0(c0) {}
     ~RealVariable()
     {
-    //   cout << "delete" << endl;
+        //   cout << "delete" << endl;
     }
     friend RealVariable &operator*(double num, RealVariable &x);
     friend RealVariable &operator^(RealVariable &x, double num);
@@ -49,26 +49,40 @@ public:
 
 // ************************* complex *************************
 
-// class ComplexVariable
-// {
-// public:
-//     ComplexVariable *adr[100] = {0};
-//     int ind = 0;
-//     double coef_2;
-//     double coef_1;
-//     complex<double> comp = 0. + 0i;
-//     ComplexVariable(double c2, double c1) : coef_2(0),
-//                                             coef_1(1) {}
-//     ComplexVariable(double c2, double c1) : coef_2(c2),
-//                                             coef_1(c1) {}
-
-//     friend ComplexVariable &operator*(double num, ComplexVariable x);
-//     friend ComplexVariable &operator^(ComplexVariable x, double num);
-//     friend ComplexVariable &operator/(ComplexVariable x, double num);
-// };
+class ComplexVariable
+{
+public:
+    ComplexVariable *add = NULL;
+    double coef_2;
+    double coef_1;
+    complex<double> comp = 0. + 0i;
+    ComplexVariable() : coef_2(0),
+                        coef_1(1) {}
+    ComplexVariable(double c2, double c1) : coef_2(c2),
+                                            coef_1(c1) {}
+    ~ComplexVariable()
+    {
+        //   cout << "delete" << endl;
+    }
+    friend ComplexVariable &operator*(double num, ComplexVariable &x);
+    friend ComplexVariable &operator^(ComplexVariable &x, double num);
+    friend ComplexVariable &operator/(ComplexVariable &x, double num);
+    friend ComplexVariable &operator+(ComplexVariable &x, double num);
+    friend ComplexVariable &operator+(double num, ComplexVariable &x);
+    friend ComplexVariable &operator-(ComplexVariable &x, double num);
+    friend ComplexVariable &operator+(ComplexVariable &x, ComplexVariable &y);
+    friend ComplexVariable &operator+(ComplexVariable &x, complex<double> y);
+    friend ComplexVariable &operator-(ComplexVariable &x, ComplexVariable &y);
+    friend ComplexVariable &operator==(ComplexVariable &x, ComplexVariable &y);
+    friend ComplexVariable &operator==(ComplexVariable &x, double num);
+    
+};
 
 double solve(RealVariable &expr);
-// complex<double> solve(ComplexVariable &expr);
+complex<double> solve(ComplexVariable &expr);
+
 } // namespace solver
+
+ostream &operator<<(ostream& os, complex<double> comp);
 
 #endif /* SOLVER_HPP_ */
