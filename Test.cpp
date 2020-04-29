367d68270eadb64ef2725b7241c8c1f584e77e48
oriel <orielmalihi1@gmail.com>
2020-04-29 14:19:36 +0300

    wip

--- Test.cpp
+++ Test.cpp
@@ -25,6 +25,9 @@ TEST_CASE("linear equation - complex")
     CHECK(solve(y == 3) == 3.+0i);
     CHECK(solve(3 == y) == 3.+0i);
     CHECK(solve(y == 3i) == 0.+3i);
+    CHECK(solve(y == 3i + 5i) == 0.+8i);
+    CHECK(solve( 3i * 5i * 2. == 2*y) == -15.+0i);
+    CHECK(solve( 3i * 5i * 2. == 2*y + y) == -10.+0i);
     CHECK(solve(y == 2.+3i) == 2.+3i);
     CHECK(solve(2.*3i + 4. == y) == 4.+6i);
 }
